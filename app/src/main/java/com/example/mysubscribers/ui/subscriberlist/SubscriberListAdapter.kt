0e8c4997c80b0df5ebd55af38c7022e388aa614a
arthu <arthurguilhermesb@gmail.com>
2022-03-27 12:19:29 -0300

    create adapter for recyclerview list of subscribers

---
+++ app/src/main/java/com/example/mysubscribers/ui/subscriberlist/SubscriberListAdapter.kt
@@ -0,0 +1,22 @@
+package com.example.mysubscribers.ui.subscriberlist
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class SubscriberListAdapter : RecyclerView.Adapter<SubscriberListAdapter.SubscriberListViewHolder>() {
+
+    class SubscriberListViewHolder(itemView:View):RecyclerView.ViewHolder(itemView){}
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SubscriberListViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: SubscriberListViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
